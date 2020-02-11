ffddb3f83f9526bc95846e4a925983f9ab9db793
Alena Kazarina <alenakazarina1@gmail.com>
2020-02-11 12:26:02 +0300

    added router

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
-import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import WelcomeScreen from '../welcome-screen/welcome-screen';
+import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen';
+import GenreQuestionScreen from '../genre-question-screen/genre-question-screen';
 
 const welcomeButtonClickHandler = () => {};
 
-const App = ({errorsCount}) => {
-  return (
-    <WelcomeScreen
-      errorsCount={errorsCount}
-      onWelcomeButtonClick={welcomeButtonClickHandler}
-    />
-  );
-};
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {errorsCount} = this.props;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <WelcomeScreen
+              errorsCount={errorsCount}
+              onWelcomeButtonClick={welcomeButtonClickHandler}
+            />
+          </Route>
+          <Route exact path="/dev-artist">
+            <ArtistQuestionScreen />
+          </Route>
+          <Route exact path="/dev-genre">
+            <GenreQuestionScreen />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
   errorsCount: PropTypes.number.isRequired
